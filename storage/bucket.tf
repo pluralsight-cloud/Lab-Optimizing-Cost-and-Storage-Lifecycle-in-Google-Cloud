edee3924a619887616a9adf2d9c6aa220331f8bd
Amina <127981554+Aminabibi12@users.noreply.github.com>
2026-02-09 11:08:52 +0100

    Update bucket.tf fixed

--- storage/bucket.tf
+++ storage/bucket.tf
@@ -7,51 +7,6 @@ resource "google_storage_bucket" "reports_bucket" {
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 
-  #########################################################
-  #  SOLUTION – Lifecycle Cost Optimization
-  #########################################################
-
-  # -------------------------------------------------------
-  # After 30 days → Move to NEARLINE
-  # -------------------------------------------------------
-  # lifecycle_rule {
-  #   action {
-  #     type          = "SetStorageClass"
-  #     storage_class = "NEARLINE"
-  #   }
-  #   condition {
-  #     age = 30
-  #   }
-  # }
-
-  # -------------------------------------------------------
-  # After 90 days → Move to COLDLINE
-  # -------------------------------------------------------
-  # lifecycle_rule {
-  #   action {
-  #     type          = "SetStorageClass"
-  #     storage_class = "COLDLINE"
-  #   }
-  #   condition {
-  #     age = 90
-  #   }
-  # }
-
-  # -------------------------------------------------------
-  # After 365 days → Move to ARCHIVE
-  # -------------------------------------------------------
-  # lifecycle_rule {
-  #   action {
-  #     type          = "SetStorageClass"
-  #     storage_class = "ARCHIVE"
-  #   }
-  #   condition {
-  #     age = 365
-  #   }
-  # }
-
-}
-
 #########################################################
 # Sample Object – Creates initial file for demonstration
 #########################################################
